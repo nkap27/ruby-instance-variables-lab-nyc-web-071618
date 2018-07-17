690f9c5af659fc5bbbecac8a1f8f27a4e94f91de
Natasha K <kapr.natasha@gmail.com>
2018-07-17 21:05:45 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+
+  #name= defines an instance variable @this_dogs_name
+
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+
+  #retrieves the value of an instance variable @this_dogs_name
+
+  def name
+    @this_dogs_name
+  end
+
+end
