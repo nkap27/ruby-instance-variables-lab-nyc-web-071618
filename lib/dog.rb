6d8c73423fca6b442d5f06fd39898b21b60e1624
Natasha K <kapr.natasha@gmail.com>
2018-07-17 21:05:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,10 @@
 class Dog
+
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+
+  def name #retrieves the value of an instance variable @this_dogs_name
+    @this_dogs_name 
+  end
 end
